7078f9bc38aeec9e678e4195a031048c922c3899
dongfangqiezi <952028898@qq.com>
2020-05-07 14:44:34 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    //  create
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    //  store   登录逻辑处理
+    public function store(Request $request)
+    {
+        //  验证用户输入信息是否合法
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        
+        //  该用户存在于数据库，且邮箱和密码相符合
+        //  根据邮箱在数据库中查找用户，如果查找到且密码的哈希值与数据库中的哈希值相匹配，那么就登录成功，反之失败。
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', '欢迎回来！');
+            //  使用 Auth::user() 方法获取用户信息传递给路由
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配！');
+            //  使用 withInput()  后模板里old('email')  将能获取到上一次用户提交的内容，这样用户就无需再次输入邮箱等内容
+            return redirect()->back()->withInput();
+        }
+
+       
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,29 @@
 */
 
 //  静态页面路由
+
+//use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
+
+
+
 Route::get("/", "StaticPagesController@home")->name('home');
 Route::get("/help", "StaticPagesController@help")->name('help');
 Route::get("/about", "StaticPagesController@about")->name('about');
 
-//  用户操作路由
+//  用户注册路由
 Route::get("/signup", "UsersController@create")->name('signup');
 
 //  RESTful用户资源路由
-Route::resource('users', 'UsersController');
+Route::resource('/users', 'UsersController');
 //Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+
+/**
+ * 用户登录处理
+ */
+//  用户登录界面
+Route::get('/login', 'SessionsController@create')->name('login');
+//  创建新会话(登录)
+Route::post('/login', 'SessionsController@store')->name('login');
+//  销毁会话(退出登录)
+Route::delete('/logout', 'SessionsController@destroy')->name('logout');
