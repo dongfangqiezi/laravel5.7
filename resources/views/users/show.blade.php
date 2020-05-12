a5262509c2c00f6b5c522386ce992ebbe4ac9f53
dongfangqiezi <952028898@qq.com>
2020-05-12 16:09:16 +0800

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,11 +9,15 @@
                 <section class="user_info">
                     @include('shared._user_info', ['user' => $user])
                 </section>
+                <section class="stats mt-2">
+                    @include('shared._stats', ['user' => $user])
+                </section>
+                <hr>
                 <section class="status">
                     @if ($statuses->count() > 0)
                     <ul class="list-unstyled">
                         @foreach ($statuses as $status)
-                        @include('statuses._status')
+                            @include('statuses._status')
                         @endforeach
                     </ul>
                     <div class="mt-5">
